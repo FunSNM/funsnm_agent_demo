8abd912ac6b277296dd7f940ea441cf34d3c01df
Anupam Prasad <anupam.13@gmail.com>
2025-10-02 10:22:55 +0200

    updated plotting and loading for Helsinki dataset

--- 01_load_data.py
+++ 01_load_data.py
@@ -4,7 +4,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 
-demo_name = "aws-iot"
+demo_name = "helsinki"
 # demo_name = "appliance"
 # demo_name = "solar_power"
 # demo_name = "water_level"
@@ -22,22 +22,27 @@ df_sensors_dict = {}
 
 ##=====LOAD DATASETS===========
 if demo_name == "helsinki":
-    sensor_keys = ["O3", "PM10", "PM25"]
     timestamp_col = "timestamp"
 
-    filenames = [
-        "J1_60.166443_24.921221_3m_q10.csv",
-        "J11_60.148267_24.919831_2m_q10.csv",
-        "J7_60.156552_24.917241_2m_q10.csv",
-        "J2_60.155524_24.915056_3m_q10.csv",
-        "J5_60.158452_24.921397_3m_q10.csv",
-    ]
+    df_sensors_dict = {}
+    sensor_coords = {}
 
-    for i, filename in enumerate(filenames):
-        df_raw = pd.read_csv(os.path.join(data_folder, filename), delimiter=";")
-        df_raw["timestamp"] = pd.to_datetime(df_raw["timestamp"])
-        sensor_name = filename.split(".")[0]
-        df_sensors_dict.update({sensor_name: df_raw})
+    for (dirpath, dirnames, filenames) in os.walk(data_folder):
+        for fname in filenames:
+            df_raw = pd.read_csv(os.path.join(data_folder, fname), delimiter=";")
+            df_raw["timestamp"] = pd.to_datetime(df_raw["timestamp"])
+            df_raw.set_index(keys='timestamp', inplace=True)
+            sensor_board_name = fname.split("_")[0]
+            sensor_board_num = '0' + sensor_board_name[1:]
+            sensor_coords[(sensor_board_name[0], sensor_board_num[-2:])] = (float(fname.split("_")[1]),
+                                                                            float(fname.split("_")[2]))
+            df_sensors_dict.update({(sensor_board_name[0], sensor_board_num[-2:]): df_raw})
+
+    df_multiindex = pd.concat(df_sensors_dict)
+    df_multiindex.index = pd.MultiIndex.from_tuples(df_multiindex.index)
+    df_multiindex.index.names = ['Cluster', 'Board', 'timestamp']
+
+    df_multiindex.sort_index(inplace=True)
 
 elif demo_name == "aws-iot":
     sensor_keys = ["co", "humidity", "lpg"]
@@ -117,24 +122,43 @@ elif demo_name == "water_level":
         df_sensors_dict.update({sensor_name: df_sensor_board})
 
 
-print(f"DATASET NAME          : {demo_name} ")
-print(f"SENSOR BOARD NAMES ({len(df_sensors_dict)}): {list(df_sensors_dict.keys())} ")
-print(f"QUANTITIES         ({len(sensor_keys)}): {sensor_keys} ")
+# print(f"DATASET NAME          : {demo_name} ")
+# print(f"SENSOR BOARD NAMES ({len(df_sensors_dict)}): {list(df_sensors_dict.keys())} ")
+# print(f"QUANTITIES         ({len(sensor_keys)}): {sensor_keys} ")
 
 ## PLOT
-n_sensor_boards = len(df_sensors_dict)
-fig, axes = plt.subplots(n_sensor_boards, 1, sharex=True, figsize=(23, 9))
-for ax, (sensor_board_name, df_sensor_dict) in zip(axes, df_sensors_dict.items()):
-    for sensor_key in sensor_keys:
-        ax.plot(
-            pd.to_datetime(df_sensor_dict[timestamp_col]).values,
-            df_sensor_dict[sensor_key].values,
-            label=sensor_key,
-        )
-    ax.grid()
-    ax.set_ylabel("Values")
-    ax.set_title(f"Sensor Board ID: {sensor_board_name}")
+if demo_name=='helsinki':
+    sensor_keys = ["O3", "PM10", "PM25"]
+    n_sensor_keys = len(sensor_keys)
+    cluster = 'V'
+    boards = df_multiindex.loc[cluster].index.unique(level='Board')
+    n_boards = len(boards)
+    fig, axes = plt.subplots(n_boards, len(sensor_keys), sharex=True, figsize=(4*n_boards, 4*n_sensor_keys))
+    for k, board in enumerate(boards):
+        for l, sensor_key in enumerate(sensor_keys):
+            axes[k][l].plot(
+               df_multiindex.loc[cluster, board].index,
+                df_multiindex.loc[cluster, board][sensor_key].values,
+                label=sensor_key,
+            )
+            axes[k][l].grid()
+            axes[k][l].set_ylabel(sensor_key)
+            axes[k][l].set_title(f"Sensor Board ID: {cluster+board}")
+else:
+    n_sensor_boards = len(df_sensors_dict)
+    fig, axes = plt.subplots(n_sensor_boards, 1, sharex=True, figsize=(23, 9))
+    for ax, (sensor_board_name, df_sensor) in zip(axes, df_sensors_dict.items()):
+        for sensor_key in sensor_keys:
+            ax.plot(
+                pd.to_datetime(df_sensor[timestamp_col]).values,
+                df_sensor[sensor_key].values,
+                label=sensor_key,
+            )
+        ax.grid()
+        ax.set_ylabel("Values")
+        ax.set_title(f"Sensor Board ID: {sensor_board_name}")
 
+plt.show()
 # axes[0].legend()
 # axes[-1].set_xlabel("Timestamps")
 # fig.tight_layout()
